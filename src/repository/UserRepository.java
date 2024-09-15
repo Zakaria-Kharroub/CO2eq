a00aca640132962b4a64e3f86270719e3dbd51af
zakaria <kharroubzakaria32@gmail.com>
2024-09-15 17:23:49 +0100

    transaction JDBC

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -9,21 +9,61 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class UserRepository {
     private Connection connection;
 
     public UserRepository(Connection connection) {
         this.connection = connection;
+
     }
+    Scanner inp = new Scanner(System.in);
+
 
     public void addUser(User user) throws SQLException {
         String query = "INSERT INTO users (name, age) VALUES (?, ?)";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setInt(2, user.getAge());
-            preparedStatement.executeUpdate();
+
+        try{
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, user.getName());
+                preparedStatement.setInt(2, user.getAge());
+
+                System.out.println("vraiment vous voulez ajouter ce user?");
+                System.out.println(" 1 - yes");
+                System.out.println(" 2 - no ");
+
+                int choix = inp.nextInt();
+
+                if (choix == 1){
+                    preparedStatement.executeUpdate();
+                    connection.commit();
+                    System.out.println("transaction confimee");
+                }else {
+                    connection.rollback();
+                    System.out.println("ajout anulle transaction rollback");
+                }
+
+
+            }
+
+        }catch (SQLException e){
+            if (connection !=null){
+                try{
+                    connection.rollback();
+                    System.out.println("error transaction rollback");
+                }catch(SQLException rolbackExcep){
+                    rolbackExcep.printStackTrace();
+                }
+            }
+//            System.out.println("error "+e.getMessage());
+
+        }finally {
+            if (connection !=null){
+                connection.setAutoCommit(true);
+            }
         }
     }
 
